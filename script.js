78da5ea1dcc7af5a5b818909b8908098dfbc0233
petars <165178919+petarskola@users.noreply.github.com>
2024-06-04 13:08:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ for(i = 0; i < cols.length; i++)
     cols[i].style.width = pixelSize+'px';
     cols[i].style.height = pixelSize+'px';
 }
-
+var numOfPixels=75*75;
+for(i= 0;i<numOfPixels;i++)
+    document.getElementById("p"+i).style.backgroundColor="#ffffff";
 function canvasChange()
 {
     size.innerHTML="Canvas Size "+canvasSize.value+"x"+canvasSize.value;
@@ -21,6 +23,7 @@ function canvasChange()
     for(var i=0;i<parseFloat(canvasSize.value)*parseFloat(canvasSize.value);i++)
     {
         pixels+="<div class=\"pixel\" id='p"+i+"'onclick=changePixelColor("+i+")  ondragover=changePixelColor("+i+")></div>";
+        numOfPixels=i+1;
     }
     pixelSize=500/parseFloat(canvasSize.value);
     canvas.innerHTML=pixels;
@@ -29,15 +32,19 @@ function canvasChange()
         cols[i].style.width = pixelSize+'px';
         cols[i].style.height = pixelSize+'px';
     }
+    for(i= 0;i<numOfPixels;i++)
+        document.getElementById("p"+i).style.backgroundColor="#ffffff";
 }
 
-var currentColor="#000000";
+var currentColor=colorInput.value;
 
 function changeColor(color)
 {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
     if(color=="rand")
+    {    
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
         currentColor="#"+randomColor;
+    }
     else
         currentColor="#ffffff";
     colorInput.value=currentColor;
@@ -47,7 +54,55 @@ function pickColor()
     currentColor = colorInput.value;
 }
 
+var x=0;
+function colorPick()
+{
+    x=1;
+}
 function changePixelColor(index)
 {
-    document.getElementById("p"+index).style.backgroundColor=currentColor;
+    if(x==1)
+    {
+        currentColor=document.getElementById("p"+index).style.backgroundColor;
+        colorInput.value=rgbToHex(currentColor);
+    }
+    else
+    {    
+        document.getElementById("p"+index).style.backgroundColor=currentColor;
+        if(brushSize.value==2)
+        {
+            document.getElementById("p"+parseInt(index-Math.sqrt(numOfPixels))).style.backgroundColor=currentColor;
+            document.getElementById("p"+parseInt(index+Math.sqrt(numOfPixels))).style.backgroundColor=currentColor;
+            document.getElementById("p"+parseInt(index-1)).style.backgroundColor=currentColor;
+            document.getElementById("p"+parseInt(index+1)).style.backgroundColor=currentColor;
+        }
+    }
+    x=0;
+}
+
+function rgbToHex(col)
+{
+    if(col.charAt(0)=='r')
+    {
+        col=col.replace('rgb(','').replace(')','').split(',');
+        var r=parseInt(col[0], 10).toString(16);
+        var g=parseInt(col[1], 10).toString(16);
+        var b=parseInt(col[2], 10).toString(16);
+        r=r.length==1?'0'+r:r; g=g.length==1?'0'+g:g; b=b.length==1?'0'+b:b;
+        var colHex='#'+r+g+b;
+        return colHex;
+    }
+}
+
+function clearCanvas()
+{
+    for(i = 0; i < cols.length; i++) 
+    {
+        cols[i].style.backgroundColor="#ffffff";
+    }
+}
+
+function brushChange()
+{
+    brush.innerHTML="Brush Size " + brushSize.value;
 }
